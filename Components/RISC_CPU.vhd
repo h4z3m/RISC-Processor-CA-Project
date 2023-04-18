1e2abc616df0eeaf7769753ac8d7273a6b1d0f72
h4z3m <h4z3m.private@gmail.com>
2023-04-18 20:31:39 +0200

    -Input/Output port & 2x1 mux

---
+++ Components/RISC_CPU.vhd
@@ -0,0 +1,11 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY RISC_CPU IS
+    PORT (
+        clk : IN STD_LOGIC;
+        reset : IN STD_LOGIC
+
+    );
+END ENTITY;
